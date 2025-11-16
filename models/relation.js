033be13e584ecf9e264fb2edefe2b37226f68562
tareq <tareqouedraogo2@gmail.com>
2025-11-16 16:43:50 -0500

    Renommage Notifications.js en Notification.js

--- models/relation.js
+++ models/relation.js
@@ -1,17 +1,4 @@
-import User from "./User.js";
 
-
-//Definition des relations plusieurs a plusieurs entre les modeles
-//DUser.belongsToMany(Role, { through: "UserRoles" }) //Un user a un role
-
-//DRole.belongsToMany(User, { through: "UserRoles" }); //Un role appartient a un user
-
-// Relations un vers plusieurs entre Department et User
-
-//DDepartment.hasMany(User)
-//DUser.belongsTo(Department)
-
-//Dexport { User, Role, Department };
 import Notification from "./Notification.js";
 import NotificationRecipient from "./NotificationRecipient.js";
 import User from "./User.js";
