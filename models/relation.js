cd72eac5b6b834837b340bfa0449c9c2299c8bb3
tareq <tareqouedraogo2@gmail.com>
2025-11-16 16:29:42 -0500

    Ajout relations Notifications et NotificationRecipient

--- models/relation.js
+++ models/relation.js
@@ -12,3 +12,34 @@ import User from "./User.js";
 //DUser.belongsTo(Department)
 
 //Dexport { User, Role, Department };
+import Notification from "./Notification.js";
+import NotificationRecipient from "./NotificationRecipient.js";
+import User from "./User.js";
+
+/**
+ * Relations pour les notifications
+ */
+
+// 1) Une notification est envoyée par un seul user (sender)
+Notification.belongsTo(User, {
+  foreignKey: "senderId",
+  as: "sender",
+});
+
+// 2) Une notification peut cibler un seul user direct
+Notification.belongsTo(User, {
+  foreignKey: "targetUserId",
+  as: "targetUser",
+});
+
+// 3) Une notification peut avoir plusieurs destinataires (pour scope = list ou chantier)
+Notification.hasMany(NotificationRecipient, {
+  foreignKey: "notificationId",
+  as: "recipients",
+});
+
+// 4) Chaque destinataire est un User
+NotificationRecipient.belongsTo(User, {
+  foreignKey: "userId",
+  as: "recipient",
+});
