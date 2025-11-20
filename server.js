07cbe5e89d9ea1225cf60b6172c5ac52541d3749
BOMBOMA Gatigbene <2739871@collegelacite.ca>
2025-11-19 20:56:32 -0500

    First commit

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 
 // ##############################################################    NOUVELLE ROUTE POUR LES INSPECTIONS
-app.use('/api/inspections', inspectionRoutes);
+app.use('/api/inspections', inspectionRoutes);``
 
 app.listen(PORT, () => {
   console.log(`Le serveur a demarre sur le port ${PORT}`);
